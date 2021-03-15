e98a11e29371975695534a09778691882d4a2ea0
StaciAF <aaen.it19@gmail.com>
2021-03-15 17:34:40 -0500

    updated solution file using TypeVar declaration

--- 0x00-python_variable_annotations/101-safely_get_value.py
+++ 0x00-python_variable_annotations/101-safely_get_value.py
@@ -3,12 +3,13 @@
 this module augments a given function with type annotation
 """
 from typing import Mapping, Any, Union, TypeVar
+T = TypeVar('T')
 
 
 def safely_get_value(dct: Mapping,
                      key: Any,
-                     default: Union[TypeVar, None] = None
-                     ) -> Union[Any, TypeVar]:
+                     default: Union[T, None] = None
+                     ) -> Union[Any, T]:
     """ this function returns key value from dict """
     if key in dct:
         return dct[key]
