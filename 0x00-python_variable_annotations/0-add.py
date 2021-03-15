8a0a10f45f7d718469983d38e65c03f3de830882
StaciAF <aaen.it19@gmail.com>
2021-03-15 12:06:53 -0500

    adds program files and main files for tasks 0 & 1, mypy and pycodestyle compatible - docstrings

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""
+this module uses type annotation to document a function that adds floats
+"""
+
+
+def add(a: float, b: float) -> float:
+    """ this function adds two floats """
+    return a + b
---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""
+this module uses type annotation to document a string concat function
+"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """ this function concats 2 strings and returns a single string """
+    return str1 + str2
