672695abda156adb7429b6fc2534991eb70c507c
StaciAF <aaen.it19@gmail.com>
2021-03-15 13:18:45 -0500

    adds solutions and main files through task 3, empty files to complete project

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""
+this module uses type annotation to document a floor function
+"""
+
+
+def floor(n: float) -> int:
+    """ this function returns the floor of the float passed """
+    return int(n)
