e8bc3380d7ab5b3a39c79e29dda75275d384adb4
StaciAF <aaen.it19@gmail.com>
2021-03-16 13:43:07 -0500

    updates method with random.uniform then removes float cast on return

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -8,6 +8,6 @@ import asyncio
 
 async def wait_random(max_delay: int = 10) -> float:
     """ this coroutine waits for a random delay then returns the delay time """
-    delay = random.randint(0, max_delay)
+    delay = random.uniform(0, max_delay)
     await asyncio.sleep(delay)
-    return float(delay)
+    return delay
