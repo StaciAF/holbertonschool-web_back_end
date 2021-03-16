328889630c7605eae33c0e69abb3685fdfc9da3b
StaciAF <aaen.it19@gmail.com>
2021-03-16 13:13:49 -0500

    adds function for task 0, getting single digit floats - unsure based on output if correct

---
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""
+this module writes an async coroutine with random delay
+"""
+import random
+import asyncio
+
+
+async def wait_random(max_delay: int = 10) -> float:
+    """ this coroutine waits for a random delay then returns the delay time """
+    delay = random.randint(0, max_delay)
+    await asyncio.sleep(delay)
+    return float(delay)
