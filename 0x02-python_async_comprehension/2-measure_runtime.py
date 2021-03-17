9490e6f32e5587c52015aded9c46c9053143497e
StaciAF <aaen.it19@gmail.com>
2021-03-17 15:57:02 -0500

    adds solution to task 2 and driver file

---
+++ 0x02-python_async_comprehension/2-measure_runtime.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+"""
+this module creates a function to measure runtime of parallel comprehensions
+"""
+import asyncio
+import time
+async_comprehension = __import__('1-async_comprehension').async_comprehension
+
+
+async def measure_runtime() -> float:
+    """ this coroutine measures runtime then returns it """
+    timeStamp1 = time.time()
+    await asyncio.gather(
+        async_comprehension(),
+        async_comprehension(),
+        async_comprehension(),       async_comprehension()
+    )
+    timeStamp2 = time.time()
+    lapsed = timeStamp2 - timeStamp1
+    return lapsed
