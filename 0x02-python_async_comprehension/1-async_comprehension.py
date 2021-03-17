9bd39056e7b01c3c76eada4f615730fc360cecf2
StaciAF <aaen.it19@gmail.com>
2021-03-17 15:40:26 -0500

    updated return type annotation from List[Any] to List[float]

--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -2,12 +2,12 @@
 """
 this module writes a coroutine and imports a second coroutine
 """
-from typing import List, Any
+from typing import List
 import asyncio
 async_generator = __import__('0-async_generator').async_generator
 
 
-async def async_comprehension() -> List[Any]:
+async def async_comprehension() -> List[float]:
     """ this coroutine comprehenses over imported coroutine """
     randos = [i async for i in async_generator()]
     return randos
