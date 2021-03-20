37a1b93cd88ff962e4d2124b829ee88c53613f62
StaciAF <aaen.it19@gmail.com>
2021-03-20 16:23:10 -0500

    adds solution and driver file for task 2

---
+++ 0x03-caching/2-lifo_cache.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+"""
+this module creates a class LIFOcache inherited from BaseCaching
+"""
+from base_caching import BaseCaching
+
+
+class LIFOCache(BaseCaching):
+    """ this class is a LAST IN FIRST OUT caching system  """
+    cache_list = []
+
+    def put(self, key, item):
+        """ edits cache based on FIFO method """
+        if key and item is not None:
+            self.cache_data[key] = item
+            if key not in self.cache_list:
+                self.cache_list.append(key)
+            if len(self.cache_data) > BaseCaching.MAX_ITEMS:
+                remove = self.cache_list.pop(-2)
+                print('DISCARD: ' + remove)
+                del self.cache_data[remove]
+
+    def get(self, key):
+        """ class method to return a value from cache dict """
+        return self.cache_data.get(key)
