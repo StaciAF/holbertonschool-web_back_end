faba9d5cd0d8890fc6edd5821e169e431de900fb
StaciAF <aaen.it19@gmail.com>
2021-03-20 14:50:29 -0500

    adds solution to task 0 with driver file

---
+++ 0x03-caching/0-basic_cache.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+"""
+this module creates a new class BasicCache inherited from BaseCaching
+"""
+from base_caching import BaseCaching
+
+
+class BasicCache(BaseCaching):
+    """ this class inherits from BaseCaching """
+
+
+    def put(self, key, item):
+        """ class method to put item in cache dict """
+        if key or item is not None:
+            self.cache_data[key] = item
+
+    def get(self, key):
+        """ class method to return a value from cache dict """
+        if key is None:
+            return None
+        if key not in self.cache_data:
+            return None
+        else:
+            return self.cache_data[key]
