9509e2f59d961d707e028f4c51d003f5e12ca8b6
StaciAF <aaen.it19@gmail.com>
2021-03-29 17:51:20 -0500

    adds solution for task 5, driver file update

---
+++ 0x05-personal_data/encrypt_password.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""
+this module encrypts a password
+"""
+import bcrypt
+
+
+def hash_password(password: str) -> str:
+    """ encrypts a password and returns its hashed version """
+    encodedPass = password.encode('utf-8')
+    hashed = bcrypt.hashpw(encodedPass, bcrypt.gensalt())
+    return hashed
