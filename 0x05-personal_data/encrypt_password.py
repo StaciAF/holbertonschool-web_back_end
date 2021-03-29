0a60cc11fe3d1b57d21da2e14285920f0d9763ec
StaciAF <aaen.it19@gmail.com>
2021-03-29 17:52:21 -0500

    updated return value to bytes

--- 0x05-personal_data/encrypt_password.py
+++ 0x05-personal_data/encrypt_password.py
@@ -5,7 +5,7 @@ this module encrypts a password
 import bcrypt
 
 
-def hash_password(password: str) -> str:
+def hash_password(password: str) -> bytes:
     """ encrypts a password and returns its hashed version """
     encodedPass = password.encode('utf-8')
     hashed = bcrypt.hashpw(encodedPass, bcrypt.gensalt())
