5b2d96c4a72b0d28ffde79167cc4dd3463b24f60
StaciAF <aaen.it19@gmail.com>
2021-03-29 17:58:22 -0500

    adds solution to task 6 in encrypt_password, updates driver file

--- 0x05-personal_data/encrypt_password.py
+++ 0x05-personal_data/encrypt_password.py
@@ -10,3 +10,12 @@ def hash_password(password: str) -> bytes:
     encodedPass = password.encode('utf-8')
     hashed = bcrypt.hashpw(encodedPass, bcrypt.gensalt())
     return hashed
+
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    """ this method checks if password is valid """
+    encodedPass = password.encode('utf-8')
+    if bcrypt.checkpw(encodedPass, hashed_password):
+        return True
+    else:
+        return False
