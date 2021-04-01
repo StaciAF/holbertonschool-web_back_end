b9e9351cfa4b38616a5d13f026b05a7079ce6a55
StaciAF <aaen.it19@gmail.com>
2021-04-01 16:18:32 -0500

    adds base64 auth to basicauth

--- 0x06-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x06-Basic_authentication/api/v1/auth/basic_auth.py
@@ -4,4 +4,16 @@ from api.v1.auth.auth import Auth
 
 
 class BasicAuth(Auth):
-    """ empty basic auth class inherits from Auth"""
+    """ this class inherits from Auth"""
+    def extract_base64_authorization_header(self,
+                                            authorization_header:
+                                            str) -> str:
+        """ this method extract base64 from BasicAuth """
+        if authorization_header is None:
+            return None
+        if isinstance(authorization_header, str) is False:
+            return None
+        if authorization_header.startswith('Basic ') is False:
+            return None
+        else:
+            return authorization_header[6:]
