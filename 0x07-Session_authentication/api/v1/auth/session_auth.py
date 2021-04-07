32bb1f7c8ded6df1bddb2aa373214491ec940d5c
StaciAF <aaen.it19@gmail.com>
2021-04-06 20:05:17 -0500

    adds user_id_for_session_id method to SessionAuth, output not exact

--- 0x07-Session_authentication/api/v1/auth/session_auth.py
+++ 0x07-Session_authentication/api/v1/auth/session_auth.py
@@ -15,5 +15,12 @@ class SessionAuth(Auth):
         if user_id is None or not isinstance(user_id, str):
             return None
         session_id = str(uuid.uuid4())
-        self.user_id_by_session_id = {session_id: user_id}
+        self.user_id_by_session_id[session_id] = user_id
         return session_id
+
+    def user_id_for_session_id(self, session_id: str = None) -> str:
+        """ this method returns a User ID based on Session ID """
+        if session_id is None or not isinstance(session_id, str):
+            return None
+        user = self.user_id_by_session_id.get(session_id)
+        return user
