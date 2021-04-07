7f2f9ebe9f3ecb53cb65bd37e8860d8571cbb0a5
StaciAF <aaen.it19@gmail.com>
2021-04-07 16:34:36 -0500

    updated conditional syntax for linter length

--- 0x07-Session_authentication/api/v1/app.py
+++ 0x07-Session_authentication/api/v1/app.py
@@ -52,8 +52,8 @@ def before_request_func():
                 '/api/v1/forbidden/',
                 '/api/v1/auth_session/login/']
     if auth is not None and auth.require_auth(request.path, pathList):
-        if auth.authorization_header(request) is None
-        and auth.session_cookie(request) is None:
+        if auth.authorization_header(request) is None and auth.session_cookie(
+                request) is None:
             return None
             abort(401)
         if auth.current_user(request) is None:
