9632905de15896d4558832ded6e8968045176047
StaciAF <aaen.it19@gmail.com>
2021-04-07 16:56:25 -0500

    fixed line continuation in app.py before_request

--- 0x07-Session_authentication/api/v1/app.py
+++ 0x07-Session_authentication/api/v1/app.py
@@ -52,13 +52,13 @@ def before_request_func():
                 '/api/v1/forbidden/',
                 '/api/v1/auth_session/login/']
     if auth is not None and auth.require_auth(request.path, pathList):
-        if auth.authorization_header(request) is None and auth.session_cookie(
-                request) is None:
-            return None
+        if auth.authorization_header(request) is None\
+                and auth.session_cookie(request) is None:
             abort(401)
-        if auth.current_user(request) is None:
+        auth_curr_user = auth.current_user(request)
+        if auth_curr_user is None:
             abort(403)
-        request.current_user = auth.current_user(request)
+        request.current_user = auth_curr_user
 
 
 if __name__ == "__main__":
