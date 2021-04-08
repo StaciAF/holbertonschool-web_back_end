ba0aecfb7a48108a0d4a5fa4aeafa4d480f6a4c8
StaciAF <aaen.it19@gmail.com>
2021-04-07 22:44:53 -0500

    checked linter styling

--- 0x07-Session_authentication/api/v1/auth/session_exp_auth.py
+++ 0x07-Session_authentication/api/v1/auth/session_exp_auth.py
@@ -8,7 +8,9 @@ class SessionExpAuth(SessionAuth):
 
     def __init__(self):
         """ this is the init method for SessionExpAuth """
+
     def create_session(self, user_id=None):
         """ this method creates a session returns its ID """
+
     def user_id_for_session_id(self, session_id=None):
         """ this method returns User id from dict """
