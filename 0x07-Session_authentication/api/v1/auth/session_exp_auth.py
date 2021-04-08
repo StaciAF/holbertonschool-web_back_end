415468584bbfffdbc7267836d1e495e3360527d9
StaciAF <aaen.it19@gmail.com>
2021-04-07 22:42:26 -0500

    add structure for task 9

---
+++ 0x07-Session_authentication/api/v1/auth/session_exp_auth.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python 3
+""" this module creates a SessionExpAuth class """
+from api.v1.auth.session_auth import SessionAuth
+
+
+class SessionExpAuth(SessionAuth):
+    """ this class inherits from SessionAuth """
+
+    def __init__(self):
+        """ this is the init method for SessionExpAuth """
+    def create_session(self, user_id=None):
+        """ this method creates a session returns its ID """
+    def user_id_for_session_id(self, session_id=None):
+        """ this method returns User id from dict """
