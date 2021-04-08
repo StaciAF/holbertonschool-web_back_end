a50552cd8c52d363a56cc020a32d4f24919e10d2
StaciAF <aaen.it19@gmail.com>
2021-04-07 20:12:00 -0500

    checked syntax and linter for both files, output incorrect

--- 0x07-Session_authentication/api/v1/views/session_auth.py
+++ 0x07-Session_authentication/api/v1/views/session_auth.py
@@ -19,18 +19,21 @@ def auth_sess_login():
 
     if u_email is None or u_email == "":
         return jsonify({"error": "email missing"}), 400
-    elif u_pwd is None or u_pwd == "":
+    if u_pwd is None or u_pwd == "":
         return jsonify({'error': 'password missing'}), 400
+
     emailSearch = User.search({'email', u_email})
+
     if emailSearch == []:
         return jsonify({"error": "no user found for this email"}), 404
+
     for user in emailSearch:
-        if not user.is_valid_password(u_pwd):
+        if user.email == u_email and not user.is_valid_password(u_pwd):
             return jsonify({"error": "wrong password"}), 401
-        else:
-            from api.v1.app import auth
-            get_sess_name = os.getenv('SESSION_NAME')
-            make_sess_id = auth.create_session(user.id)
-            response = jsonify(user.to_json())
-            response.set_cookie(get_sess_name, make_sess_id)
-            return response
+    else:
+        from api.v1.app import auth
+        get_sess_name = os.getenv('SESSION_NAME')
+        make_sess_id = auth.create_session(user.id)
+        response = jsonify(user.to_json())
+        response.set_cookie(get_sess_name, make_sess_id)
+        return response
