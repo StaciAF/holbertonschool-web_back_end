47bd66eebb8faaef5f668769498ead6a1a9c2695
StaciAF <aaen.it19@gmail.com>
2021-04-11 17:20:29 -0500

    removes TypeVar as return in add_user

--- 0x08-user_authentication_service/db.py
+++ 0x08-user_authentication_service/db.py
@@ -29,7 +29,7 @@ class DB:
             self.__session = DBSession()
         return self.__session
 
-    def add_user(self, email: str, hashed_password: str) -> TypeVar('User'):
+    def add_user(self, email: str, hashed_password: str) -> User:
         """ this method saves a user to db then returns a User object """
         new_sess = self._session
         new_user = User(email=email, hashed_password=hashed_password)
