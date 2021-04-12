23b7603e6dcc6f45092a3b11cb99b1fa28bc6ab3
StaciAF <aaen.it19@gmail.com>
2021-04-12 12:12:52 -0500

    removing error handling in find_user_by // using attr methods for update_user dict

--- 0x08-user_authentication_service/db.py
+++ 0x08-user_authentication_service/db.py
@@ -3,17 +3,16 @@
 this module creats a new class DB
 """
 from sqlalchemy import create_engine
-from sqlalchemy.exc import NoResultFound, InvalidRequestError
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm.exc import NoResultFound
 
 
-from sys import argv
-from typing import List, TypeVar
 from user import Base, User
 
 
 class DB:
+
     def __init__(self):
         self._engine = create_engine("sqlite:///a.db", echo=False)
         Base.metadata.drop_all(self._engine)
@@ -30,45 +29,26 @@ class DB:
     def add_user(self, email: str, hashed_password: str) -> User:
         """ this method saves a user to db then returns a User object """
         session = self._session
-        if email and hashed_password:
-            new_user = User(email=email, hashed_password=hashed_password)
-            session.add(new_user)
-            session.commit()
-            return new_user
-        return None
+        new_user = User(email=email, hashed_password=hashed_password)
+        session.add(new_user)
+        session.commit()
+        return new_user
 
     def find_user_by(self, **kwargs) -> User:
         """ this method takes in keyword args and returns match from list """
         session = self._session
-        user_attrs = ['id',
-                      'email',
-                      'hashed_password',
-                      'session_id',
-                      'reset_token']
-        for key in kwargs.items():
-            try:
-                key in user_attrs
-            except InvalidRequestError:
-                pass
-            user_found = session.query(User).filter_by(**kwargs).first()
-            if user_found is None:
-                raise NoResultFound
-            else:
-                return user_found
+        user_found = session.query(User).filter_by(**kwargs).one()
+        return user_found
 
     def update_user(self, user_id: int, **kwargs) -> None:
         """ this method finds_user_by and updates user attrs """
-        user_attrs = ['id',
-                      'email',
-                      'hashed_password',
-                      'session_id',
-                      'reset_token']
         session = self._session
         this_user = self.find_user_by(id=user_id)
         for key, val in kwargs.items():
-            if key not in user_attrs:
+            if hasattr(this_user, key):
+                setattr(this_user, key, val)
+                session.commit()
+                return None
+            else:
                 raise ValueError
-            this_user.key = val
-            session.add(this_user)
-            session.commit()
         return None
