c08418a12cc61fce56573c74d78b069f471ab78b
StaciAF <aaen.it19@gmail.com>
2021-04-15 18:49:05 -0500

    adds /sessions end-point with login function

--- 0x08-user_authentication_service/app.py
+++ 0x08-user_authentication_service/app.py
@@ -3,7 +3,7 @@
 this module sets up Flask app, adds routes
 """
 from auth import Auth
-from flask import Flask, jsonify, request
+from flask import Flask, abort, jsonify, request
 
 app = Flask(__name__)
 AUTH = Auth()
@@ -31,6 +31,22 @@ def users():
             return jsonify({"email": email, "message": "user created"})
     except ValueError:
         return jsonify({"message": "email already registered"}), 400
+
+
+@app.route('/sessions', methods=['POST'], strict_slashes=False)
+def login():
+    """ this method creates a new session for the user,
+    stores it, returns JSON payload """
+    email = request.form.get('email')
+    password = request.form.get('password')
+    validated = AUTH.valid_login(email, password)
+    if validated:
+        session_id = AUTH.create_session(email)
+        response = jsonify({"email": email, "message": "logged in"})
+        response.set_cookie('session_id', session_id)
+        return response
+    else:
+        abort(401)
 
 
 if __name__ == "__main__":
