1899201811b53ebcedf33731b0d9caae4d1ebebe
StaciAF <aaen.it19@gmail.com>
2021-04-18 15:17:51 -0500

    adds test for access_nested_map, issues importing parameterized

---
+++ 0x09-Unittests_and_integration_tests/test_utils.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+""" this module builds test for utils """
+from parameterized import parameterized
+from utils import access_nested_map
+import unittest
+
+
+class TestAccessNestedMaps(unittest.TestCase):
+    """ this class tests access_nested_maps method from utils.py """
+    @parameterized.expand([
+                            ({"a": 1}, ("a",), 1),
+                            ({"a": {"b": 2}}, ("a"), {"b": 2}),
+                            ({"a": {"b": 2}}, ("a", "b"), 2)
+                          ])
+    def test_access_nested_map(self, nested_map, path, expected):
+        self.assertEqual(access_nested_map(nested_map, path), expected)
+
+
+if __name__ == '__main__':
+    unittest.main()
