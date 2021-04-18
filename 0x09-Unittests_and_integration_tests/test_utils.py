9e5e4f47471c84ede397a8f71fc0f692b025aacc
StaciAF <aaen.it19@gmail.com>
2021-04-18 17:08:29 -0500

    updates with initial thoughts for testing get_json, incomplete // circle back

--- 0x09-Unittests_and_integration_tests/test_utils.py
+++ 0x09-Unittests_and_integration_tests/test_utils.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
 """ this module builds test for utils """
 from parameterized import parameterized
-from utils import access_nested_map
+from unittest import mock
+from utils import access_nested_map, get_json
 import unittest
 
 
@@ -23,6 +24,21 @@ class TestAccessNestedMap(unittest.TestCase):
     def test_access_nested_map_exception(self, nested_map, path):
         """ this method runs test on method exceptions """
         self.assertRaises(KeyError, access_nested_map, nested_map, path)
+
+
+class TestGetJson(unittest.TestCase):
+    """ this class tests get_json method """
+    @parameterized.expand([
+        ("http://example.com", {"payload": True}),
+        ("http://holberton.io", {"payload": False})
+    ])
+    def test_get_json(self, test_url, test_payload):
+        with mock.patch('requests.get()') as mock_req:
+            response = mock_req.get_json(test_url)
+            self.assertEqual(isinstance(response, dict), True)
+            self.assertEqual(response, test_payload)
+            mock.return_value = response
+            assert mock_req() == test_payload
 
 
 if __name__ == '__main__':
