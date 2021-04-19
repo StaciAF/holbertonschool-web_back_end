90608a9af245490c8d71cd3e18d55ccd09966981
StaciAF <aaen.it19@gmail.com>
2021-04-19 18:50:34 -0500

    adds Github test module with OrgClient test

---
+++ 0x09-Unittests_and_integration_tests/test_client.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+""" this module tests the client module """
+from client import GitHubOrgClient, get_json
+from parameterized import parameterized
+from unittest.mock import patch
+import unittest
+
+
+class TestGithubOrgClient(unittest.TestCase):
+    """ this class test client methods """
+    @parameterized.expand([
+        ('google'),
+        ('abc')
+    ])
+    @patch('client.get_json', return_value={'key': 'value'})
+    def test_org(self, org_param, mock_json):
+        """ this method tests return value for client method """
+        test_org = GitHubOrgClient(org_param)
+        git_url = "https://api.github.com/orgs/" + org_param
+        self.assertEqual(test_org.org, {'key': 'value'})
+        mock_json.assert_called_once_with(git_url)
