2a75beb1138e7bc4e68b20e3697d63eff4ae5f56
StaciAF <aaen.it19@gmail.com>
2021-04-19 19:39:09 -0500

    adds test for client public_repos

--- 0x09-Unittests_and_integration_tests/test_client.py
+++ 0x09-Unittests_and_integration_tests/test_client.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python3
 """ this module tests the client module """
-from client import GitHubOrgClient
+from client import GithubOrgClient
 from parameterized import parameterized
 import unittest
-from unittest.mock import patch
+from unittest.mock import patch, PropertyMock
 
 
 class TestGithubOrgClient(unittest.TestCase):
@@ -15,7 +15,15 @@ class TestGithubOrgClient(unittest.TestCase):
     @patch('client.get_json', return_value={'key': 'value'})
     def test_org(self, org_param, mock_json):
         """ this method tests return value for client method """
-        test_org = GitHubOrgClient(org_param)
+        test_org = GithubOrgClient(org_param)
         git_url = "https://api.github.com/orgs/" + org_param
         self.assertEqual(test_org.org, {'key': 'value'})
         mock_json.assert_called_once_with(git_url)
+
+    def test_public_repos_url(self):
+        """ this method unit-tests _public_repos_url """
+        with patch('client.GithubOrgClient.org',
+                   new_callable=PropertyMock) as mock_org:
+            mock_org.return_value = {'repos_url': 'something.com'}
+            client = GithubOrgClient("brand_new")
+            self.assertEqual(client._public_repos_url, "something.com")
