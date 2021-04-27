509eee973cd58f2ac41867b7128ce8ca69fb330c
StaciAF <aaen.it19@gmail.com>
2021-04-27 14:23:19 -0500

    adds methods Cache.get Cache.get_str Cache.get_int with optional callable to convert data

--- 0x0B_redis_basic/exercise.py
+++ 0x0B_redis_basic/exercise.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 """ this module sets up REDIS """
-from typing import Union
+from typing import Callable, Union
 import redis
 import uuid
 
@@ -17,3 +17,20 @@ class Cache():
         key_gen = str(uuid.uuid4())
         self._redis.set(key_gen, data)
         return key_gen
+
+    def get(self, key: str,
+            fn: Callable = None) -> Union[str, bytes, int, float]:
+        """ this method gets value of passed key from Redis """
+        if fn is not None:
+            get_value = self._redis.get(key)
+            return fn(get_value)
+        else:
+            return get_value
+
+    def get_str(self, key: str) -> str:
+        """ this method ensures key is of type str with typecast """
+        return self.get(key, str)
+
+    def get_int(self, key: str) -> int:
+        """ this method ensure key is of type int with typecase """
+        return self.get(key, int)
