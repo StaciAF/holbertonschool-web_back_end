f12059adc2a8cab747b30eb258222a70f11a1bd9
StaciAF <aaen.it19@gmail.com>
2021-04-28 18:03:17 -0500

    adds MySQL script

---
+++ 0x0C-MySQL_Adavanced/0-uniq_users.sql
@@ -0,0 +1,7 @@
+-- script creates a user table
+-- if table exists do not fail
+CREATE TABLE IF NOT EXISTS users (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    name VARCHAR(255)
+);
