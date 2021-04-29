156b60e86c1274e2accbc005b3ee24a2da3846dd
StaciAF <aaen.it19@gmail.com>
2021-04-29 17:06:57 -0500

    added s to file name

---
+++ 0x0C-MySQL_Advanced/8-index_my_names.sql
@@ -0,0 +1,12 @@
+-- this script creates a stored procedure
+-- computes and stores avg student score
+DELIMITER $$
+CREATE PROCEDURE ComputeAverageScoreForUser(IN user_id INT)
+BEGIN
+    UPDATE users
+    SET @average = (SELECT AVG(score)
+    FROM corrections
+    WHERE corrections.user_id = user_id)
+    WHERE id = user_id;
+END$$
+DELIMITER $$;
