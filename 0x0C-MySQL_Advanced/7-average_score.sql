a253696527e18370f17db8e73884a31126a2ea66
StaciAF <aaen.it19@gmail.com>
2021-04-29 16:51:59 -0500

    adds solution task 7, last completed - partial files beyond

---
+++ 0x0C-MySQL_Advanced/7-average_score.sql
@@ -0,0 +1,14 @@
+-- this script creates a stored procedure
+-- computes and stores avg student score
+DELIMITER $$
+CREATE PROCEDURE ComputeAverageScoreForUser(IN user_id INT)
+BEGIN
+    SET @average = (
+    SELECT AVG(score)
+    FROM corrections
+    WHERE corrections.user_id = user_id);
+UPDATE users
+SET avg_score = @average
+WHERE id = user_id;
+END$$
+DELIMITER $$;
