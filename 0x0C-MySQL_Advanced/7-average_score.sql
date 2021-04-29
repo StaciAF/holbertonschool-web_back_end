b583f9ddf9543f2bd3c39801585be79cc0963461
StaciAF <aaen.it19@gmail.com>
2021-04-29 17:01:23 -0500

    updated spacing and tied two statements to eachother

--- 0x0C-MySQL_Advanced/7-average_score.sql
+++ 0x0C-MySQL_Advanced/7-average_score.sql
@@ -3,15 +3,10 @@
 DELIMITER $$
 CREATE PROCEDURE ComputeAverageScoreForUser(IN user_id INT)
 BEGIN
-    SET @average = (
-        SELECT AVG(score)
-        FROM corrections
-        WHERE corrections.user_id = user_id);
-
     UPDATE users
-    SET avg_score = @average
+    SET @average = (SELECT AVG(score)
+    FROM corrections
+    WHERE corrections.user_id = user_id)
     WHERE id = user_id;
-
 END$$
-
 DELIMITER $$;
