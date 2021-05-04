5d6546366b4ace099df16c985a762b75564c9329
StaciAF <aaen.it19@gmail.com>
2021-05-04 13:17:41 -0500

    adds script to create index using first character

--- 0x0C-MySQL_Advanced/8-index_my_names.sql
+++ 0x0C-MySQL_Advanced/8-index_my_names.sql
@@ -1,12 +1,3 @@
 -- this script creates a stored procedure
 -- computes and stores avg student score
-DELIMITER $$
-CREATE PROCEDURE ComputeAverageScoreForUser(IN user_id INT)
-BEGIN
-    UPDATE users
-    SET @average = (SELECT AVG(score)
-    FROM corrections
-    WHERE corrections.user_id = user_id)
-    WHERE id = user_id;
-END$$
-DELIMITER $$;
+CREATE INDEX idx_name_first ON names (name(1));
