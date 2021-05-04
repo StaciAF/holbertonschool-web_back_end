d153346c3d3961b9281dd16cd0ef813aa789503c
StaciAF <aaen.it19@gmail.com>
2021-05-04 13:08:49 -0500

    updates MySQL keyword order

--- 0x0C-MySQL_Advanced/7-average_score.sql
+++ 0x0C-MySQL_Advanced/7-average_score.sql
@@ -3,12 +3,14 @@
 DELIMITER $$
 CREATE PROCEDURE ComputeAverageScoreForUser (IN user_id INT)
 BEGIN
-    
-    SET @average = (SELECT AVG(score)
-    FROM corrections
-    WHERE corrections.user_id = user_id)
+    SET @average = (
+        SELECT AVG(score)
+        FROM corrections
+        WHERE corrections.user_id = user_id)
+
     UPDATE users
     SET average_score = @average
     WHERE id = user_id;
 END$$
-DELIMITER $$;
+
+DELIMITER ;
