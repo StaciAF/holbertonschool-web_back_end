a261f0c05e1c787a4859e1ff7ca92ddb68c05cd7
StaciAF <aaen.it19@gmail.com>
2021-05-04 13:05:45 -0500

    updates syntax order, assigns average_score to average

--- 0x0C-MySQL_Advanced/7-average_score.sql
+++ 0x0C-MySQL_Advanced/7-average_score.sql
@@ -3,10 +3,12 @@
 DELIMITER $$
 CREATE PROCEDURE ComputeAverageScoreForUser (IN user_id INT)
 BEGIN
-    UPDATE users
+    
     SET @average = (SELECT AVG(score)
     FROM corrections
     WHERE corrections.user_id = user_id)
+    UPDATE users
+    SET average_score = @average
     WHERE id = user_id;
 END$$
 DELIMITER $$;
