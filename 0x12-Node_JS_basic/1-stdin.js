8cf59fdf8adeaa0933a1ecc4604d30600d723811
StaciAF <aaen.it19@gmail.com>
2021-05-23 21:34:26 +0000

    adds solution using process -derp

--- 0x12-Node_JS_basic/1-stdin.js
+++ 0x12-Node_JS_basic/1-stdin.js
@@ -1,15 +1,15 @@
-const readline = require('readline').createInterface({
-  input: process.stdin,
-  output: process.stdout,
+process.stdout.write(
+  'Weclome to Holberton School, what is your name?\n',
+);
+process.stdin.setEncoding('utf8');
+process.stdin.on('readable', () => {
+  const done = process.stdin.read();
+
+  if (done) {
+    process.stdout.write(`Your name is: ${done}`);
+  }
 });
 
-readline.setPrompt('Welcome to Holberton School, what is your name?\n');
-readline.prompt();
-readline.on('line', (name) => {
-  console.log(`Your name is: ${name}`);
-  readline.close();
-});
-readline.on('close', () => {
-  console.log('This important software is now closing ');
-  process.exit(0);
+process.stdin.on('end', () => {
+  process.stdout.write('This important software is now closing\n');
 });
