19ef79d5252d3651330634d92f55a391927a14b9
StaciAF <aaen.it19@gmail.com>
2021-05-24 17:14:28 +0000

    adds solution to task 3

---
+++ 0x12-Node_JS_basic/3-read_file_async.js
@@ -0,0 +1,23 @@
+const fs = require('fs').promises;
+
+const countStudents = async (path) => {
+  if (!fs.existsSync(path)) {
+    throw new Error('Cannot load the database');
+  }
+  const content = await fs.readFile(path, 'utf8');
+  let lines = content.toString().split(/\r?\n/);
+  lines = lines.filter((line) => line !== '');
+  lines.shift();
+  console.log(`Number of students: ${lines.length}`);
+  const findCS = lines.filter((line) => line.endsWith('CS')).map((line) => {
+    const stdntCS = line.split(',');
+    return stdntCS[0];
+  });
+  console.log(`Number of students in CS: ${findCS.length}. List: ${findCS.join(', ')}`);
+  const findSWE = lines.filter((line) => line.endsWith('SWE')).map((line) => {
+    const stdntSWE = line.split(',');
+    return stdntSWE[0];
+  });
+  console.log(`Number of students in SWE: ${findSWE.length}. List: ${findSWE.join(', ')}`);
+};
+module.exports = countStudents;
