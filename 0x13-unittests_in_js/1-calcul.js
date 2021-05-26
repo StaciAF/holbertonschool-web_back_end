a0844e3af3453dd50a5e1632e67e8c8295acbe13
StaciAF <aaen.it19@gmail.com>
2021-05-26 02:45:07 +0000

    adds testing suite for tasks 0 & 1

---
+++ 0x13-unittests_in_js/1-calcul.js
@@ -0,0 +1,14 @@
+const calculateNumber = (type, a, b) => {
+    if (type === 'SUM') {
+	return Math.round(a) + Math.round(b);
+    }
+    if (type === 'SUBTRACT') {
+	return Math.round(a) - Math.round(b);
+    }
+    if (type === 'DIVIDE' && b === 0) {
+	return 'Error';
+    } else {
+	return Math.round(a) / Math.round(b);
+    }
+}
+module.exports = calculateNumber;
