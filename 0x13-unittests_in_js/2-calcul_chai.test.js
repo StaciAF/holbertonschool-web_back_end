6f6356298b87b7090394af2060a4f7cb2bd74002
StaciAF <aaen.it19@gmail.com>
2021-05-26 19:40:07 +0000

    adds updated test for task 2

--- 0x13-unittests_in_js/2-calcul_chai.test.js
+++ 0x13-unittests_in_js/2-calcul_chai.test.js
@@ -1,17 +1,18 @@
-const assert = require('assert');
-const calculateNumber = require('./1-calcul.js');
+const chai = require('chai');
+const expect = chai.expect;
+const calculateNumber = require('./2-calcul_chai.js');
 
 describe('#calculateNumber()', function () {
     it('should return 6 when calculateNumber is passed SUM, 1.4, and 4.5', function () {
-        assert.strictEqual(calculateNumber('SUM', 1.4, 4.5), 6);
+        expect(calculateNumber('SUM', 1.4, 4.5)).to.equal(6);
     });
     it('should return -4 when calculateNumber is passed SUBTRACT, 1.4 and 4.5', function () {
-        assert.strictEqual(calculateNumber('SUBTRACT', 1.4, 4.5), -4);
+        expect(calculateNumber('SUBTRACT', 1.4, 4.5)).to.equal(-4);
     });
     it('should return 0.2 when calculateNumber is passed DIVIDE, 1.4, and 4.5', function () {
-        assert.strictEqual(calculateNumber('DIVIDE', 1.4, 4.5), 0.2);
+        expect(calculateNumber('DIVIDE', 1.4, 4.5)).to.equal(0.2);
     });
     it('should return Error when calculateNumber is passed DIVIDE, 1.4, and 0', function () {
-        assert.strictEqual(calculateNumber('DIVIDE', 1.4, 0), 'Error');
+        expect(calculateNumber('DIVIDE', 1.4, 0)).to.equal('Error');
     });
-});
+  });
