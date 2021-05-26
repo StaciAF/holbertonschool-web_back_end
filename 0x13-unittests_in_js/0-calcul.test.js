adf9ac496e32a0cba31a3997c56de12a3abf3376
StaciAF <aaen.it19@gmail.com>
2021-05-26 19:09:41 +0000

    changed assert to equal from strictEqual

--- 0x13-unittests_in_js/0-calcul.test.js
+++ 0x13-unittests_in_js/0-calcul.test.js
@@ -3,16 +3,16 @@ const calculateNumber = require('./0-calcul.js');
 
 describe('#calculateNumber()', function () {
     it('should return 4 when calculateNumber is passed 1 and 3', function () {
-        assert.strictEqual(calculateNumber(1, 3), 4);
+        assert.equal(calculateNumber(1, 3), 4);
     });
     it('should return 5 when calculateNumber is passed 1 and 3.7', function () {
-      assert.strictEqual(calculateNumber(1, 3.7), 5);
+      assert.equal(calculateNumber(1, 3.7), 5);
     });
     it('should return 5 when calculateNumber is passed 1.2 and 3.7', function () {
-      assert.strictEqual(calculateNumber(1.2, 3.7), 5);
+      assert.equal(calculateNumber(1.2, 3.7), 5);
     });
     it('should return 6 when calculateNumber is passed 1.5 and 3.7', function () {
-      assert.strictEqual(calculateNumber(1.5, 3.7), 6);
+      assert.equal(calculateNumber(1.5, 3.7), 6);
     });
   });
   
