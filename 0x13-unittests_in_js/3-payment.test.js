987102fd0d618f04470ae0d3106c1737664d8bec
StaciAF <aaen.it19@gmail.com>
2021-05-26 22:16:17 +0000

    updates test suite

--- 0x13-unittests_in_js/3-payment.test.js
+++ 0x13-unittests_in_js/3-payment.test.js
@@ -1,17 +1,15 @@
-const chai = require('chai');
-const expect = chai.expect;
-const assert = require('assert');
+const chai = require("chai");
+const should = chai.should();
+const sinon = require("sinon");
+const Utils = require("./utils");
+const sendPaymentRequestToApi = require("./3-payment.js");
 
-const sinon = require('sinon');
-const spy = sinon.spy;
-const Utils = require('./utils');
-const sendPaymentRequestToApi = require('./3-payment.js');
+describe("#sendPaymentRequestToApi()", function () {
+  it("should check functionality of Utils.calculateNumber()", function () {
+    const sendSpy = sinon.spy(Utils, "calculateNumber");
+    sendPaymentRequestToApi(100, 20);
 
-describe('sendPaymentRequestToApi', function () {
-    it('should check functionality of Utils.calculateNumber()', function() {
-	const sendSpy = spy(Utils, "calculateNumber");
-	sendPaymentRequestToApi(100, 20);
-	expect(sendSpy.calledWith('SUM', 100, 20)).to.equal(true);
-	sendSpy.restore();
-    })
+    sendSpy.calledWith("SUM", 100, 20).should.equal(true);
+    sendSpy.restore();
+  });
 });
