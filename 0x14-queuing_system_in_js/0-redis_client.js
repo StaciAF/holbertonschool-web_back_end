8e326c8f4dece9ffc871efbf2cb988f575675ee7
StaciAF <aaen.it19@gmail.com>
2021-05-31 18:52:26 +0000

    adds solution to task 1, never saw ERROR print

---
+++ 0x14-queuing_system_in_js/0-redis_client.js
@@ -0,0 +1,9 @@
+import redis from 'redis'
+const client = redis.createClient()
+
+client.on('error', function(err) {
+    console.error(`Redis client not connected to the server: ${err}`);
+});
+client.on('connect', function() {
+    console.log('Redis client connected to the server');
+});
